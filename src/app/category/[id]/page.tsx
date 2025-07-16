1ae9dfb4b80ed60e80f0df3efe80421144f3d743
Seijyu Kondo <kondo@reflection-inc.com>
2025-07-16 17:46:03 +0900

    fix: any回避のためparams型を明示しESLint対応

--- src/app/category/[id]/page.tsx
+++ src/app/category/[id]/page.tsx
@@ -14,8 +14,12 @@ type ImageType = {
   usage?: string | string[];
 };
 
-export default async function CategoryPage(props: any) {
-  const id = props?.params?.id;
+export default async function CategoryPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const { id } = params;
 
   const category = await client.get({ endpoint: 'categories', contentId: id });
   const data = await client.get({
